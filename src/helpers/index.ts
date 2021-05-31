e5638ca9f55e0c1a260693ac8e476a06fdbe820e
Imre Kiss <jakab.asd@gmail.com>
2021-05-31 19:01:51 +0200

    Add docs to the helper functions

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,12 +2,26 @@ import { REQUIRED_REGEX, REQUIRED_ERROR } from '../constants';
 import { ActionTypes } from '../enums';
 import type { ValidationError, InvalidValidatorIndex, ValidationSchema, State, Actions, Validator } from '../types';
 
+/**
+ * Gets the actual value and the error and returns the final error string.
+ *
+ * @param value Actual state value to hand over function bsed errors.
+ * @param error Actual string or function that returns with an error string.
+ * @returns Error string.
+ */
 export const getErrorString = <T>(value: T, error?: ValidationError<T>): string => {
   if (typeof error === 'function') return error(value);
   if (typeof error === 'string') return error;
   return '';
 };
 
+/**
+ * Returns the index of the validator function or regexp that is failing on the actual state value.
+ *
+ * @param value Actual state value to hand over to the given validators.
+ * @param validators List of functions or regexps to check the validity of the actual state value.
+ * @returns Invalid validator index number or null.
+ */
 export const getInvalidValidatorIndex = <T>(value: T, validators: Validator<T>[]): InvalidValidatorIndex => {
   const invalidIndex = validators.findIndex(validator => {
     if (typeof validator === 'function') return !validator(value);
@@ -18,6 +32,13 @@ export const getInvalidValidatorIndex = <T>(value: T, validators: Validator<T>[]
   return invalidIndex !== -1 ? invalidIndex : null;
 };
 
+/**
+ * Returns with the error that the user provided for the corresponding validator based on array index.
+ *
+ * @param value Actual state value to work with.
+ * @param validationSchema Validators and errors list to work with.
+ * @returns Validation error string or empty string.
+ */
 export const getValidationError = <T>(value: T, validationSchema: ValidationSchema<T>): string => {
   const { required, requiredError, validators, errors } = validationSchema;
   if (required && ((typeof value === 'string' && !REQUIRED_REGEX.test(value)) || value === null || value === undefined))
@@ -29,8 +50,22 @@ export const getValidationError = <T>(value: T, validationSchema: ValidationSche
   return '';
 };
 
+/**
+ * Returns or resets the initial state based on the given initial value.
+ *
+ * @param arg Initial value.
+ * @returns Initial state with value and error props.
+ */
 export const initalizer = <T>(arg: T): State<T> => ({ value: arg, error: '' });
 
+/**
+ * Executes the incoming action on the previous state and creates a new one
+ * or keep it in case of non matching action.
+ *
+ * @param state Previous state.
+ * @param action Action to execute.
+ * @returns A new state.
+ */
 export const reducer = <T>(state: State<T>, action: Actions<T>): State<T> => {
   switch (action.type) {
     case ActionTypes.Reset:
